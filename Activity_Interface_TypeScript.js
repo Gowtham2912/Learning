d1ba4274a9305d3cf240a0d21929ac81c3337b3d
Gowtham2912 <69888695+Gowtham2912@users.noreply.github.com>
2021-04-01 11:56:15 +0530

    Update Activity_Interface_TypeScript.js
    implements and extends method example

--- Activity_Interface_TypeScript.js
+++ Activity_Interface_TypeScript.js
@@ -9,17 +9,22 @@ var Person = /** @class */ (function () {
         return "This Person name is " + this.name + " and belongs to the " + this.group + " group";
     };
     Person.prototype.printGrade = function () {
-        if (this.marks > 90)
-            return "This Person score " + this.marks + " and got Grade A";
-        else
+        if (this.marks > 90 && this.marks <= 95) {
+            return "This Person scored " + this.marks + " and got Grade A";
+        }
+        else if (this.marks > 95) {
+            return "This Person scored " + this.marks + " and got Grade S and he " + (this.isTopper ? 'is' : 'is not') + " the topper of section";
+        }
+        else {
             return "This Person soored " + this.marks + " and got Grade B";
+        }
     };
-    Person.prototype.printTopper = function () {
-        return "THis Person " + (this.isTopper ? 'is' : 'is not') + " topper";
+    Person.prototype.printMarks = function () {
+        console.log(this.marks);
     };
     return Person;
 }());
-var person = new Person('Java Script', 'Gowtham', 90, true);
+var person = new Person('Java Script', 'Gowtham', 97, false);
 console.log(person.printPerson());
 console.log(person.printGrade());
-console.log(person.printTopper());
+person.printMarks();
